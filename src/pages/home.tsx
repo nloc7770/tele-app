9c7561ac9b724fc76c61b4f82e322d0b9ee36dec
hainh <hainh.blackwind@gmail.com>
2023-03-12 00:51:16 +0700

    commit update code import contract

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,10 @@
 import { useAuth } from '@/context/auth';
 import { useState } from 'react';
+import { TelegramClient } from 'telegram';
+import { generateRandomBytes, readBigIntFromBuffer } from 'telegram/Helpers';
+import { StringSession } from 'telegram/sessions';
 import { Api } from 'telegram/tl/api';
+
 
 export default function index() {
     const [file, setFile] = useState();
@@ -11,43 +15,48 @@ export default function index() {
     //     setFile(e.target.files[0]);
     // };
     const handleOnSubmit = async () => {
-        try {
-            const result = await client.invoke(
-                new Api.contacts.AddContact({
-                    id: user.username,
-                    firstName: "loc",
-                    lastName: "nguyen",
-                    phone: "+84347779264",
-                    addPhonePrivacyException: true,
-                })
-            );
-            console.log(result); // prints the result
+
+        await client.connect(); // This assumes you have already authenticated with .start()
+        const result = await client.invoke(
+            new Api.contacts.ImportContacts({
+                contacts: [
+                    new Api.InputPhoneContact({
+                        clientId: readBigIntFromBuffer(generateRandomBytes(8)),
+                        phone: "+84347779264",
+                        firstName: "Test Loc Them tư contract",
+                        lastName: "ádasdas",
+                    }),
+                ],
+            })
+        )
+
+        console.log(result); // prints the result
         // e.preventDefault(); if (file) {
         //     fileReader.onload = function (event: any) {
         //         const csvOutput = event.target.result;
         //     };
         //     fileReader.readAsText(file);
         // }
-        } catch (error) {
-            console.log(error);
-        }
+
     };
     return (
-        <div className='w-full h-full'><div style={{ textAlign: "center" }}>
+        <div ><div style={{ textAlign: "center" }}>
             <h1>Import tele-script </h1>
-                {/* <input
+            {/* <input
                     type={"file"}
                     id={"csvFileInput"}
                     accept={".csv"}
                     onChange={handleOnChange}
                 /> */}
-                <button
-                    onClick={() => {
-                        handleOnSubmit();
-                    }}
-                >
-                    IMPORT CSV
-                </button>
+
+            <button
+                type='button'
+                onClick={() => {
+                    handleOnSubmit();
+                }}
+            >
+                IMPORT CSV
+            </button>
         </div></div>
     )
 }
