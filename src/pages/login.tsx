9faae4a60ce3f85cbb0b7ba93f800e38f5319573
hainh <hainh.blackwind@gmail.com>
2023-03-13 07:58:28 +0700

    a

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -67,7 +67,7 @@ const Login = () => {
                                         <label className="block text-gray-700 text-sm font-bold mb-2" >
                                             Số điện thoại
                                         </label>
-                                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="phone" type="text" placeholder="phone" />
+                                        <input style={{backgroundColor:'white'}} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="phone" type="text" placeholder="phone" />
                                     </div>
                                     {/* <div className="mb-6">
                                         <label className="block text-gray-700 text-sm font-bold mb-2" >
