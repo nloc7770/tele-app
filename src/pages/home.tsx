849376787138a3d85112b689f1b5cddf6775d047
“nloc7770” <nloc7770@gmail.com>
2023-03-29 10:52:44 +0700

    update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -138,14 +138,14 @@ export default function index() {
         toggleToast({
             show: true,
             status: "warning",
-            message: "Vui lòng chờ 1 phút để tiến trình tiếp tục!",
-            time: 60000,
+            message: "Vui lòng chờ 30 giây để tiến trình tiếp tục!",
+            time:30000,
         });
         setTimeout(async () => {
             if (data?.length !== number) {
                 await handleAddContact(number + 1)
             }
-        }, 60000);
+        }, 30000);
        
     }
 
