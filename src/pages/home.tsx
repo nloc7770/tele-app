9f4d838d74c493ec89d44b79751d9e8e50790d56
locnd2905 <locnd@galaxy.com.vn>
2023-05-07 00:25:49 +0700

    add block user

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -25,6 +25,7 @@ export default function index() {
     const [pageRunning, setPageRunning] = useState<number>(0)
     const [isImport, setIsImport] = useState<boolean>(false)
     const [totalData, setTotalData] = useState<number>(0);
+    const [isActive, setIsActive] = useState<boolean>(true);
 
     const init = async () => {
         const result = await client.invoke(
@@ -36,7 +37,12 @@ export default function index() {
             .select('*')
             .eq('phone', user?.phone)
             .gt('created_at', moment().format("YYYY-MM-DD"));
-        setIsImport(dataRes ? dataRes[0].is_import : false)
+        setIsImport((dataRes && dataRes.length > 0) ? dataRes[0].is_import : false)
+        const { data: dataLock } = await supabase
+            .from('key')
+            .select('*')
+            .eq('phone', user?.phone)
+        setIsActive((dataLock && dataLock.length > 0) ? dataLock[0].is_active : false)
     }
     useEffect(() => {
         init()
@@ -181,99 +187,98 @@ export default function index() {
     }
 
     return (
-
         <div className='flex flex-col justify-center items-center w-full'>
-            <h1>Import tele-script </h1>
-            {data?.length > 0 && <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
-                <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${pageRunning * 100 / data?.length}%` }}>{(pageRunning * 100 / data?.length).toFixed(2)}%</div>
-            </div>}
-            <div className='my-5 self-end flex'>
-                <label className="p-3 border-2 rounded-lg mr-2 cursor-pointer ">
-                    <div
-                    >
-                        Tổng số liên hệ đã thêm : {totalData}
-                    </div>
-                </label>
-                <label className="p-3 border-2 rounded-lg bg-blue-200 hover:bg-blue-400 cursor-pointer ">
-                    <input
-                        onChange={
-                            handleOnChange
-                        }
-                        id="csvInput"
-                        name="file"
-                        type="File"
-                        accept={".csv"}
-                    />
-
-                    Lấy danh sách liên hệ
-                </label>
-                <label className="p-3 border-2 rounded-lg bg-blue-300 hover:bg-blue-500 cursor-pointer ">
-                    <div
-                        onClick={() => {
-                            handleAddContact(0)
-                        }}
-                    >
-                        Thêm liên hệ
-                    </div>
-                </label>
-            </div>
-            <div className="relative items-center block w-full p-6 bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-800 dark:hover:bg-gray-700">
-                <table className={`${loading && "opacity-20"}`}>
-                    <thead>
-                        <tr>
-                            <th>STT</th>
-                            <th>Số điện thoại</th>
-                            <th>Họ</th>
-                            <th>Tên</th>
-                            <th>Trạng thái</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data && data[pageActive]?.length > 0 && data[pageActive]?.map((item: any, index: any) => {
-                            return (
-                                item?.phone && <tr key={index} >
-                                    <th>{item?.index}</th>
-                                    <th>{item?.phone}</th>
-                                    <th>{item?.firstName}</th>
-                                    <th>{item?.lastName}</th>
-                                    <th>{item?.status == 1 ? "Thành công" : item.status == 0 ? "Chưa xử lý" : "Thất bại"}</th>
-                                </tr>
-                            )
-                        }
-                        )}
-                    </tbody>
-                </table>
-                {loading && <div role="status" className="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2 w-full h-full bg-transparent flex justify-center">
-                    <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/4000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" /><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" /></svg>
-                    <span className="sr-only">Loading...</span>
+            {isActive ? <> <h1>Import tele-script </h1>
+                {data?.length > 0 && <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                    <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${pageRunning * 100 / data?.length}%` }}>{(pageRunning * 100 / data?.length).toFixed(2)}%</div>
                 </div>}
-            </div>
-            {data && data?.length > 0 && <div className="flex items-center space-x-1 self-end mt-3">
-                <a onClick={() => {
-                    if (pageActive != 0) {
-                        setPageActive(pageActive - 1)
-                    }
-                }} className="flex items-center px-4 py-2 text-gray-500 bg-gray-300 rounded-md">
-                    Trang trước
-                </a>
-                {/* {data?.map((item: any, index: any) => {
-                    return */}
-                <a key={index + "itemsss"} onClick={() => {
-                }} className={`px-4 py-2 text-gray-700 bg-gray-200 rounded-md hover:bg-blue-400 hover:text-white`}>
-                    {pageActive + 1 + "/" + data?.length}
-                </a>
-                {/* }
-                )} */}
-                <a
-                    onClick={() => {
-                        if (pageActive + 1 < data?.length) {
-                            setPageActive(pageActive + 1)
+                <div className='my-5 self-end flex'>
+                    <label className="p-3 border-2 rounded-lg mr-2 cursor-pointer ">
+                        <div
+                        >
+                            Tổng số liên hệ đã thêm : {totalData}
+                        </div>
+                    </label>
+                    <label className="p-3 border-2 rounded-lg bg-blue-200 hover:bg-blue-400 cursor-pointer ">
+                        <input
+                            onChange={
+                                handleOnChange
+                            }
+                            id="csvInput"
+                            name="file"
+                            type="File"
+                            accept={".csv"}
+                        />
+
+                        Lấy danh sách liên hệ
+                    </label>
+                    <label className="p-3 border-2 rounded-lg bg-blue-300 hover:bg-blue-500 cursor-pointer ">
+                        <div
+                            onClick={() => {
+                                handleAddContact(0)
+                            }}
+                        >
+                            Thêm liên hệ
+                        </div>
+                    </label>
+                </div>
+                <div className="relative items-center block w-full p-6 bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-800 dark:hover:bg-gray-700">
+                    <table className={`${loading && "opacity-20"}`}>
+                        <thead>
+                            <tr>
+                                <th>STT</th>
+                                <th>Số điện thoại</th>
+                                <th>Họ</th>
+                                <th>Tên</th>
+                                <th>Trạng thái</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data && data[pageActive]?.length > 0 && data[pageActive]?.map((item: any, index: any) => {
+                                return (
+                                    item?.phone && <tr key={index} >
+                                        <th>{item?.index}</th>
+                                        <th>{item?.phone}</th>
+                                        <th>{item?.firstName}</th>
+                                        <th>{item?.lastName}</th>
+                                        <th>{item?.status == 1 ? "Thành công" : item.status == 0 ? "Chưa xử lý" : "Thất bại"}</th>
+                                    </tr>
+                                )
+                            }
+                            )}
+                        </tbody>
+                    </table>
+                    {loading && <div role="status" className="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2 w-full h-full bg-transparent flex justify-center">
+                        <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/4000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" /><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" /></svg>
+                        <span className="sr-only">Loading...</span>
+                    </div>}
+                </div>
+                {data && data?.length > 0 && <div className="flex items-center space-x-1 self-end mt-3">
+                    <a onClick={() => {
+                        if (pageActive != 0) {
+                            setPageActive(pageActive - 1)
                         }
-                    }} className="px-4 py-2 font-bold text-gray-500 bg-gray-300 rounded-md hover:bg-blue-400 hover:text-white">
-                    Trang sau
-                </a>
-            </div>
-            }
+                    }} className="flex items-center px-4 py-2 text-gray-500 bg-gray-300 rounded-md">
+                        Trang trước
+                    </a>
+                    {/* {data?.map((item: any, index: any) => {
+                    return */}
+                    <a key={index + "itemsss"} onClick={() => {
+                    }} className={`px-4 py-2 text-gray-700 bg-gray-200 rounded-md hover:bg-blue-400 hover:text-white`}>
+                        {pageActive + 1 + "/" + data?.length}
+                    </a>
+                    {/* }
+                )} */}
+                    <a
+                        onClick={() => {
+                            if (pageActive + 1 < data?.length) {
+                                setPageActive(pageActive + 1)
+                            }
+                        }} className="px-4 py-2 font-bold text-gray-500 bg-gray-300 rounded-md hover:bg-blue-400 hover:text-white">
+                        Trang sau
+                    </a>
+                </div>
+                }</> : <p>Tài khoản bị khóa</p>}
         </div>
 
     )
